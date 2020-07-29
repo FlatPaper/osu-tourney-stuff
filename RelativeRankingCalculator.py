acc8fa21ec7876f787988d99cc85bea82ea8c4db
Jonathan Sumabat <diaxaxx@gmail.com>
2020-07-29 16:39:53 -0400

    Index player id's to usernames in a lobby

--- RelativeRankingCalculator.py
+++ RelativeRankingCalculator.py
@@ -10,6 +10,7 @@ class RelativeRanking:
     USER_URL = "https://osu.ppy.sh/api/get_user"
     MULTI_URL = "https://osu.ppy.sh/api/get_match"
     REQUEST_DELAY = 0.7
+    player_index = {}
 
     def __init__(self, links):
         self.links = links
@@ -62,3 +63,22 @@ class RelativeRanking:
         data = json.dumps(data)
         data = json.loads(data)
         return data
+
+    def match_relative_ranking(self, multi_link):
+        """
+        :param multi_link: link to the lobby
+        :return: absolutely nothing yet
+        """
+        match_id = str(multi_link).strip().split("/")[-1]
+
+        match_info = self.get_match_info(match_id)
+
+        # let us index the player id's first
+        for game in match_info['games']:
+            for score in game['scores']:
+                user_id = str(score['user_id'])
+                if user_id not in self.player_index:
+                    self.player_index[user_id] = self.id_to_username(user_id)
+
+    def get_player_index(self):
+        return "Participants: " + str(self.player_index)
