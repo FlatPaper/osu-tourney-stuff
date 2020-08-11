fd5c05c00a92427885cfde3f3e9873770cb58342
Jonathan Sumabat <diaxaxx@gmail.com>
2020-08-11 18:46:18 -0400

    Current commands

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
-import discord
+import discord, json
+from RelativeRankingCalculator import RelativeRanking
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,5 +23,19 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+
+    if str(message.content) == "!xenopaper":
+        await message.channel.send("https://media.discordapp.net/attachments/731987457880490154/742601523430686781/unknown.png")
+
+    if str(message.content) == "!namderanker":
+        await message.channel.send("https://cdn.discordapp.com/attachments/724332647505199216/742762469654790194/image0.png")
+
+    if message.author.id != 258630920024621069:
+        return
+
+    if str(message.content).startswith("!calc"):
+        calculator = RelativeRanking(message)
+        await calculator.run()
+
 
 client.run(TOKEN)
